95c755324201096348ca92a709e8ec280337007e
KRANTI@45 <krantiendolkar@gmail.com>
2024-04-17 11:39:03 +0530

    .

--- Day1/q7.py
+++ Day1/q7.py
@@ -1,8 +1,11 @@
 #7) Using for loop, write and run a Python program to find factorial from 0 to 10.
 #num=[0,1,2,3,4,5,6,7,8,9,10]
-num=(input("Enter the num:"))
+def fact(num):
+    if num==0:
+        return 1
+    return num*fact(num-1)
 
-for value  in num:
-    fact=value*(value-1)
-print(fact)
-    
+for i in range (1,11):
+   fact(i)
+   print ( f"factorial of {i} is:{fact(i)}")
+
